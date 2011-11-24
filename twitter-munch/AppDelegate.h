30fc76b0fc4a17de7773e2422ebedb74c2c1de9e
Anthony Perritano <aperritano@gmail.com>
2011-11-24 14:33:35 +0100

    Initial Commit

---
+++ twitter-munch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  twitter-munch
+//
+//  Created by Anthony Perritano on 11/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ twitter-munch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  twitter-munch
+//
+//  Created by Anthony Perritano on 11/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
